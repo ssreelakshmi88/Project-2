655bd0cfefb1c28d9b59e7f69fe4b5c169ab2ff4
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-04-22 22:20:40 +0000

    Created question array in quiz.js

---
+++ assets/js/quiz.js
@@ -0,0 +1,57 @@
+let puzzleContainer = [{
+        puzzle: 'What was the destination of the missing flight MH370?',
+        choice: ['Kuala Lumpur', 'Beijing', 'Singapore', 'Tokyo'],
+        answer: 'Beijing'
+    },
+
+    {
+        puzzle: 'Who is the founder of &quot;The Lego Group&quot;?',
+        choice: ['Gerhardt Kirk Christiansen', 'Kirstine Christiansen', 'Jens Niels Christiansen', 'Tokyo'],
+        answer: 'Ole Kirk Christiansen'
+    },
+    {
+        puzzle: 'What is the last letter of the Greek alphabet?',
+        choice: ['Omega', 'Mu', 'Epsilon', 'Kappa'],
+        answer: 'Omega'
+    },
+    {
+        puzzle: 'The new One World Trade Center in Manhattan, New York City was designed by which architect? ',
+        choice: ['Bjarke Ingels', 'David Childs', 'Michael Arad', 'Fumihiko Maki'],
+        answer: 'David Childs'
+    },
+    {
+        puzzle: 'Amsterdam Centraal station is twinned with what station? ',
+        choice: ['London Liverpool ', 'Paris Gare du Nord', 'Brussels Midi', 'Frankfurt (Main) Hauptbahnhof'],
+        answer: 'London Liverpool '
+    },
+    {
+        puzzle: 'When did the website &quot;Facebook&quot; launch? ',
+        choice: ['2005 ', '2003', '2004', '2006'],
+        answer: '2004 '
+    },
+    {
+        puzzle: 'Which slogan did the fast food company, McDonald&#039;s, use before their &quot;I&#039;m Lovin&#039; It&quot; slogan?" ',
+        choice: ['We Love to See You Smile ', 'Why Pay More!?', 'Have It Your Way', 'Making People Happy Through Food'],
+        answer: 'We Love to See You Smile'
+    },
+    {
+        puzzle: 'Which country drives on the left side of the road?',
+        choice: ['Germany ', 'Russia', 'Japan', 'China'],
+        answer: 'Japan'
+    },
+    {
+        puzzle: 'Which mathematician refused the Fields Medal?',
+
+        choice: ['Grigori Perelman', 'Andrew Wiles', 'Edward Witten', 'Terence Taoa'],
+        answer: 'Grigori Perelman'
+    },
+    {
+        puzzle: 'What are the three starter Pokemon available in Pokemon Black and White?',
+
+        choice: ['Snivy, Fennekin, Froakie', 'Snivy, Tepig, Oshawotts', 'Chespin, Tepig, Froakie', 'Chespin, Fennekin, Oshawott'],
+        answer: 'Snivy, Tepig, Oshawott'
+    },
+]
+
+let pContainer = document.getElementById('puzzle-container');
+let puzzle = document.getElementById('puzzles');
