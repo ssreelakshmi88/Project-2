eb33fb6c67e00b64bba8bb7bb598a8d97e5ac492
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-05-02 09:40:05 +0000

    Added media queries in files help.css and contact.css

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,58 +6,30 @@ let questionBank = 0;
 let currentQuestion = {};
 let numberQuestions = [];
 let addquestions = [];
-
-
-fetch(
-        'https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple')
-
-    .then((res) => {
-        return res.json();
-    })
-    .then((showQuestions) => {
-
-        addquestions = showQuestions.results.map((showQuestions) => {
-            const modifiedQuestion = {
-                question: showQuestions.question,
-            };
-
-            const possibleAnswers = [...showQuestions.incorrect_answers];
-            modifiedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            possibleAnswers.splice(
-
-                modifiedQuestion.answer - 1,
-                0,
-                showQuestions.correct_answer
-            );
-
-            possibleAnswers.forEach((option, index) => {
-                modifiedQuestion['option' + (index + 1)] = option;
-            });
-            return modifiedQuestion;
-        });
-
-        startGame();
-
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
-
-
-function startGame() {
-    questionBank = 0;
-    points = 0;
-    numberQuestions = [...addquestions];
-    getNewQuestion();
-};
-
+let questionsList = [];
 const MAX_QUESTIONS = 20;
 
-function getNewQuestion() {
-    if (numberQuestions.length === 0 || questionBank >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentpoints', points);
-        return window.location.assign('/game.html');
-    }
+let showQuestions = () => {
+    addquestions = showQuestions.results.map((showQuestions) => {
+        const modifiedQuestion = {
+            question: showQuestions.question,
+        };
+
+        const possibleAnswers = [...showQuestions.incorrect_answers];
+        modifiedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+        possibleAnswers.splice(
+
+            modifiedQuestion.answer - 1,
+            0,
+            showQuestions.correct_answer
+        );
+
+        possibleAnswers.forEach((option, index) => {
+            modifiedQuestion['option' + (index + 1)] = option;
+        });
+        return modifiedQuestion;
+    });
+
+    startGame();
+
 }
-questionBank++;
