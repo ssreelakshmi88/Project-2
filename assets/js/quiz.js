5c49fa9cfa2799783573898e146828b1cc555ef6
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-05-02 10:48:39 +0000

    Functions added to display questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,35 +1,93 @@
 const question = document.getElementById('question');
-const options = Array.from(document.getElementsByClassName('option-text'));
+
 let numberAnswers = false;
 let points = 0;
 let questionBank = 0;
+let formattedQuestions = [];
+let correctAnswers = [];
+let wrongAnswers = [];
 let currentQuestion = {};
 let numberQuestions = [];
 let addquestions = [];
 let questionsList = [];
 const MAX_QUESTIONS = 20;
 
-let showQuestions = () => {
-    addquestions = showQuestions.results.map((showQuestions) => {
-        const modifiedQuestion = {
-            question: showQuestions.question,
-        };
+document.addEventListener("DOMContentLoaded", function () {
+    const options = document.getElementsByClassName('option-text');
 
-        const possibleAnswers = [...showQuestions.incorrect_answers];
-        modifiedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-        possibleAnswers.splice(
-
-            modifiedQuestion.answer - 1,
-            0,
-            showQuestions.correct_answer
-        );
-
-        possibleAnswers.forEach((option, index) => {
-            modifiedQuestion['option' + (index + 1)] = option;
+    for (let option of options) {
+        option.addEventListener("click", function () {
+            let answer = this.getAttribute("data-type");
+            checkAnswer(answer);
         });
-        return modifiedQuestion;
+    }
+
+
+    //EventListener for next question
+
+    document.getElementById('next').addEventListener("click", function () {
+        enableButtons();
+
+        if (questionBank < 20) {
+            questionBank++;
+            showQuestion(questionBank);
+        } else {
+
+            showScoreBoard();
+        }
+
     });
 
     startGame();
+});
 
+
+function startGame() {
+    questionBank = 0;
+    points = 0;
+    numberQuestions = [...addquestions];
+    renderNewQuestion();
+};
+
+function renderNewQuestion() {
+    if (numberQuestions.length === 0 || questionBank >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentpoints', points);
+        return window.location.assign('/game.html');
+    }
+    questionBank++;
+}
+
+// load question on DOM load
+let preLoadedQuestions = () => {
+    fetch('https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple')
+        .then((res) => {
+            questionsList = res.json();
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    preLoadedQuestions();
+});
+
+function showQuestion(questionBank) {
+
+    document.getElementById("question-game").innerText =
+
+        formattedQuestions[questionBank].question;
+}
+
+document.getElementById("choice-1").innerHTML =
+    formattedQuestions[questionBank].choice[0];
+document.getElementById("choice-2").innerHTML =
+    formattedQuestions[questionBank].choice[1];
+document.getElementById("choice-3").innerHTML =
+    formattedQuestions[questionBank].choice[2];
+document.getElementById("choice-4").innerHTML =
+    formattedQuestions[questionBank].choice[3];
+
+if (questionBank === 9) {
+    document.getElementById("next").innerHTML = "Get Result";
 }
