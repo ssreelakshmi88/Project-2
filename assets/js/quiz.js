badcda6c98c000a2ec4668be0b95137c41ba5221
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-05-04 09:27:50 +0000

    Rewrite quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,104 +1,99 @@
-const question = document.getElementById('question');
-const options = Array.from(document.getElementsByClassName('option-text'));
-let numberAnswers = false;
-let points = 0;
-let questionIndex = 0;
-let currentQuestion = {};
-let numberQuestions = [];
-let addquestions = [];
-let questionsList = [];
-
-const MAX_QUESTIONS = 20;
-
-
-
-
-
-let showQuestions = () => {
-    addquestions = showQuestions.results.map((showQuestions) => {
-        const modifiedQuestion = {
-            question: showQuestions.question,
-        };
-
-        const possibleAnswers = [...showQuestions.incorrect_answers];
-        modifiedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-        possibleAnswers.splice(
-
-            modifiedQuestion.answer - 1,
-            0,
-            showQuestions.correct_answer
-        );
-
-        possibleAnswers.forEach((option, index) => {
-            modifiedQuestion['option' + (index + 1)] = option;
-        });
-        return modifiedQuestion;
-    });
-
-    startGame();
-
-}
-
-function displayQuestions() {
-
-
-    question.textContent = questionsList[questionIndex];
-    //console.log(questionsList[questionIndex]);
-
-}
-
-// start game
-function startGame() {
-    questionIndex = 0;
-    points = 0;
-    //numberQuestions = [...addquestions];
-    numberQuestions = questionsList.length;
-    console.log(questionsList);
-    //renderNewQuestion();
-    displayQuestions();
-
-
-
-};
-
-// render new question
-function renderNewQuestion() {
-    if (numberQuestions.length === 0 || questionIndex >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentpoints', points);
-        return window.location.assign('/quiz-result.html');
+var questionBank = [{
+        question: 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
+        option: ['Asia', 'Africa', 'Europe', 'Australia'],
+        answer: 'Africa'
+    },
+    {
+        question: 'Garampani sanctuary is located at',
+        option: ['Junagarh, Gujarat', 'Diphu, Assam', 'Kohima, Nagaland', 'Gangtok, Sikkim'],
+        answer: 'Diphu, Assam'
+    },
+    {
+        question: 'For which of the following disciplines is Nobel Prize awarded?',
+        option: ['Physics and Chemistry', 'Physiology or Medicine', 'Literature, Peace and Economics', 'All of the above'],
+        answer: 'All of the above'
+    },
+    {
+        question: 'Hitler party which came into power in 1933 is known as',
+        option: ['Labour Party', 'Nazi Party', 'Ku-Klux-Klan', 'Democratic Party'],
+        answer: 'Nazi Party'
+    },
+    {
+        question: 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
+        option: ['1967', '1968', '1958', '1922'],
+        answer: '1967'
     }
-    questionIndex++;
+]
+
+var question = document.getElementById('question');
+var quizBox = document.getElementById('quiz-box');
+var scorecard = document.getElementById('scorecard');
+var option0 = document.getElementById('option0');
+var option1 = document.getElementById('option1');
+var option2 = document.getElementById('option2');
+var option3 = document.getElementById('option3');
+var next = document.querySelector('.next');
+var points = document.getElementById('score');
+var span = document.querySelectorAll('span');
+var i = 0;
+var score = 0;
+
+//function to display questions
+function displayQuestion() {
+    for (var a = 0; a < span.length; a++) {
+        span[a].style.background = 'none';
+    }
+    question.innerHTML = 'Q.' + (i + 1) + ' ' + questionBank[i].question;
+    option0.innerHTML = questionBank[i].option[0];
+    option1.innerHTML = questionBank[i].option[1];
+    option2.innerHTML = questionBank[i].option[2];
+    option3.innerHTML = questionBank[i].option[3];
+    progress.innerHTML = "Question" + ' ' + (i + 1) + ' ' + 'of' + ' ' + questionBank.length;
 }
 
-// load question on DOM load
-let preLoadedQuestions = () => {
+//function to calculate scores
+function calcScore(e) {
+    if (e.innerHTML === questionBank[i].answer && score < questionBank.length) {
+        score = score + 1;
+        document.getElementById(e.id).style.background = 'limegreen';
+    } else {
+        document.getElementById(e.id).style.background = 'tomato';
+    }
+    setTimeout(nextQuestion, 300);
+}
 
-    fetch('https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple')
-        .then((res) => {
-            questionsList = res.json();
-            console.log(questionsList.results);
+//function to display next question
+function nextQuestion() {
+    if (i < questionBank.length - 1) {
+        i = i + 1;
+        displayQuestion();
+    } else {
+        points.innerHTML = score + '/' + questionBank.length;
+        quizContainer.style.display = 'none';
+        scoreboard.style.display = 'block'
+    }
+}
 
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+//click events to next button
+next.addEventListener('click', nextQuestion);
 
+//Back to Quiz button event
+function backToQuiz() {
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer() {
+    var answerBank = document.getElementById('answerBank');
+    var answers = document.getElementById('answers');
+    answerBank.style.display = 'block';
+    scoreboard.style.display = 'none';
+    for (var a = 0; a < questionBank.length; a++) {
+        var list = document.createElement('li');
+        list.innerHTML = questionBank[a].answer;
+        answers.appendChild(list);
+    }
 }
 
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    //preLoadedQuestions();
-    //console.log(questionsList);
-
-
-
-});
-
-window.addEventListener('load', () => {
-    preLoadedQuestions();
-    console.log(questionsList);
-    startGame();
-
-});
+displayQuestion();
