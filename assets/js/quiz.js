0782f5cce79e560162bc28599df763c0840b7a14
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-05-04 18:10:34 +0000

    Added class names in quiz.html

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,4 @@
-var questionBank = [{
+let questionBank = [{
         question: 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
         option: ['Asia', 'Africa', 'Europe', 'Australia'],
         answer: 'Africa'
@@ -25,27 +25,28 @@ var questionBank = [{
     }
 ]
 
-var question = document.getElementById('question');
-var quizBox = document.getElementById('quiz-box');
-var scorecard = document.getElementById('scorecard');
+let question = document.getElementById('question');
+let quizBox = document.getElementById('quiz-box');
+let scorecard = document.getElementById('scorecard');
+let scoreBoard = document.getElementById('scoreboard');
 let progressText = document.getElementById("progressText");
 let progressBarFull = document.getElementById("progressBarFull");
-var option0 = document.getElementById('option0');
-var option1 = document.getElementById('option1');
-var option2 = document.getElementById('option2');
-var option3 = document.getElementById('option3');
-var next = document.querySelector('.next');
-var points = document.getElementById('score');
-var span = document.querySelectorAll('span');
-var i = 0;
-var score = 0;
+let option0 = document.getElementById('option0');
+let option1 = document.getElementById('option1');
+let option2 = document.getElementById('option2');
+let option3 = document.getElementById('option3');
+let next = document.querySelector('.next');
+let points = document.getElementById('score');
+let span = document.querySelectorAll('span');
+let i = 0;
+let score = 0;
 
 //function to display questions
 function displayQuestion() {
-    for (var a = 0; a < span.length; a++) {
-        span[a].style.background = 'none';
+    for (var x = 0; x < span.length; x++) {
+        span[x].style.background = 'none';
     }
-    question.innerHTML = 'Q.' + (i + 1) + ' ' + questionBank[i].question;
+    question.innerHTML = 'No.' + (i + 1) + ' ' + questionBank[i].question;
     option0.innerHTML = questionBank[i].option[0];
     option1.innerHTML = questionBank[i].option[1];
     option2.innerHTML = questionBank[i].option[2];
@@ -61,9 +62,9 @@ function displayQuestion() {
 function calcScore(e) {
     if (e.innerHTML === questionBank[i].answer && score < questionBank.length) {
         score = score + 1;
-        document.getElementById(e.id).style.background = 'limegreen';
+        document.getElementById(e.id).style.background = 'green';
     } else {
-        document.getElementById(e.id).style.background = 'tomato';
+        document.getElementById(e.id).style.background = 'red';
     }
     setTimeout(nextQuestion, 300);
 }
